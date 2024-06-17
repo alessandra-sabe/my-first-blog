ad0ec04af241ec0709da780f3db83af1b0005c42
sabealessandra <alessandrasabe06@gmail.com>
2024-06-17 15:44:14 +0200

    Update

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,6 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts, 'alessandra': 2024}) 
+    return render(request, 'blog/post_list.html', {'posts': posts, 'alessandra': 2025}) 
 
 # Create your views here.
